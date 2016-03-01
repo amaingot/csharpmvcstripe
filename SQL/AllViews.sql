63895263db3496b162de9236ae414fed7c5bc256
Alex Maingot <alex.maingot@gmail.com>
2016-03-01 02:55:27 -0600

    Adding SQL Helper scripts

---
+++ SQL/AllViews.sql
@@ -0,0 +1,42 @@
+USE [mvcstripe_db]
+GO
+
+SELECT *
+  FROM [dbo].[AspNetRoles]
+GO
+
+USE [mvcstripe_db]
+GO
+
+SELECT *
+  FROM [dbo].[AspNetUsers]
+GO
+
+
+USE [mvcstripe_db]
+GO
+
+SELECT *
+  FROM [dbo].[AspNetUserClaims]
+GO
+
+USE [mvcstripe_db]
+GO
+
+SELECT *
+  FROM [dbo].[AspNetUserLogins]
+GO
+
+USE [mvcstripe_db]
+GO
+
+SELECT *
+  FROM [dbo].[AspNetUserRoles]
+GO
+
+USE [mvcstripe_db]
+GO
+
+SELECT *
+  FROM [dbo].[Subscriptions]
+GO
---
+++ SQL/DeleteUsers.sql
@@ -0,0 +1,25 @@
+USE [mvcstripe_db]
+GO
+
+DELETE FROM [dbo].[AspNetUserRoles]
+      
+GO
+
+
+USE [mvcstripe_db]
+GO
+
+DELETE FROM [dbo].[AspNetRoles]
+      
+GO
+
+
+
+USE [mvcstripe_db]
+GO
+
+DELETE FROM [dbo].[AspNetUsers]
+      
+GO
+
+
